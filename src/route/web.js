128e32014e81f7307327aaf51ef05bbb7b4217d3
d2412916 <nguyenvandung86420@gmail.com>
2024-02-01 16:56:15 +0700

    Add MVC

--- src/route/web.js
+++ src/route/web.js
@@ -1,11 +1,12 @@
 import express from "express";
+import homeController from "../controllers/homeController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get("/", (req, res) => {
-        return res.send("Hello world with dungnguyen");
-    });
+    router.get("/", homeController.getHomePage);
+
+
     return app.use("/", router);
 }
 
