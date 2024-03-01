f4b558cca8c16103977a03634ef95654c80e82ec
d2412916 <nguyenvandung86420@gmail.com>
2024-03-01 16:20:56 +0700

    Add database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,24 @@
+import db from "../models/index"
 
-let getHomePage = (req, res) => {
-    return res.render('homepage.ejs');
+let getHomePage = async (req, res) => {
+    try {
+        let data = await db.User.findAll();
+        console.log("data", data)
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data)
+        });
+    }
+    catch (e) {
+        console.log(e)
+    }
+
+}
+
+let getAboutPage = (req, res) => {
+    return res.render('test/about.ejs');
 }
 
 module.exports = {
     getHomePage: getHomePage,
+    getAboutPage: getAboutPage
 }
